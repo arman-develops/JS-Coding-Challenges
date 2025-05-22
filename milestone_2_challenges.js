650974960a13ecc2d0422b2de487ac7f0bb9c0f7
arman-develops <amon.victor.develops@gmail.com>
2025-05-22 13:16:10 +0300

    Completed Challenge 2: Find Maximum Value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,4 +1,4 @@
-//challenge 1: Sum Of Positives
+// Challenge 1: Sum Of Positives
 function sumOfPositives(numArray) {
     let total = 0;
     for (let i = 0; i < numArray.length; i++) {
@@ -9,3 +9,15 @@ function sumOfPositives(numArray) {
     return total;
 }
 // console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+// Challenge 2: FInd Maximum Value
+function findMax(numArray) {
+    let max = 0;
+    for (let i = 0; i < numArray.length; i++) {
+        if(numArray[i] > numArray[i + 1]) {
+            max = numArray[i];
+        }
+    }
+    return max;
+}
+// console.log(findMax([3, 7, 2, 9, 5]));
