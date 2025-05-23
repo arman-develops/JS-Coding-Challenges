3ba8319be288f7d96a19cb424bd84969d73502fb
Amon Victor <amon.victor.develops@gmail.com>
2025-05-23 03:34:40 +0300

    Completed Challenge 7: Sum Of Even Numbers

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -72,3 +72,15 @@ function filterByLength(arr, len) {
     return filteredArray;
 }
 // console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5));
+
+// Challenge 7: Sum of Even Numbers
+function sumEvenNumbers(numArray) {
+    let sum = 0;
+    for(let i = 0; i < numArray.length; i++) {
+        if(numArray[i] % 2 == 0) {
+            sum += numArray[i];
+        }
+    }
+    return sum;
+}
+// console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6]));
